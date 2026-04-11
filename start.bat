2cc33daabb5aa6c85991f46a9b63f2f56e32b761
Rafael Oliveira <rafael.oliveira789whd@gmail.com>
2026-04-11 17:43:06 -0300

    Atualizando URL do tunnel

--- start.bat
+++ start.bat
@@ -40,7 +40,7 @@ echo ================================
 echo Atualizando script.js...
 echo ================================
 
-powershell -Command "(Get-Content js/script.js) -replace 'URL_AQUI', '%URL%' | Set-Content js/script.js"
+powershell -Command "(Get-Content js/scripts.js) -replace 'URL_AQUI', '%URL%' | Set-Content js/scripts.js"
 
 echo ================================
 echo Enviando para GitHub...
